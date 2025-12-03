2e83f281b704d4d7590d330ff5c4f5841d8a5984
Nounou-93420 <ait.noury@outlook.fr>
2025-12-03 10:35:40 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -29,7 +29,10 @@ def mongraphique():
 @app.route("/histogramme/")
 def monhistogramme():
     return render_template("histogramme.html")
-  
+
+@app.route("/commits/")
+def MaPremiereAPI():
+    return render_template("commits.html")
                                                                                                                                        
 @app.route('/')
 def hello_world():
